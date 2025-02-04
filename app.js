1036c749773828f1966fa038b2987e32e2245f27
DyNis999 <dennisechague112@gmail.com>
2025-02-04 23:33:37 +0800

    Deploy lang

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 // const server = http.createServer(app);
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  // origin: "http://localhost:3000",
   credentials: true,
 }));
 
